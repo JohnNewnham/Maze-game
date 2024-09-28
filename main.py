dfcc7334d9a7402882071f77e4d048ab44ca3e7f
Johnnewnham <johnenewnham@gmail.com>
2024-09-28 17:40:29 +0100

    Added basic gameplay.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
-from py_objects import Gridcell, Grid, Movement
-from misc_funcs import generate_grid, shift_origin
+from py_objects import *
+from misc_funcs import *
 
 debug: bool = False
 
-def draw_maze() -> None:
+def drawMaze() -> None:
     for x, y in grid.iterable():
-        gridcell: Gridcell = grid.get_cell((x, y))
+        gridcell: Gridcell = grid.getCell((x, y))
         # Each cell contains information on whether there is not a wall to its right and bottom only.
         if (x==0): display.blit(left_img, (64*x-32, 64*y-32))
         if (y==0): display.blit(top_img, (64*x-32, 64*y-32))
@@ -20,51 +20,61 @@ def draw_maze() -> None:
             match gridcell.bottom:
                 case 1: display.blit(bottom_arrow, (64*x-32, 64*y-32))
                 case -1: display.blit(top_arrow, (64*x-32, 64*y-32))
+    display.blit(goal_img, (64*goal_pos[0], 64*goal_pos[1]))
+    display.blit(player_img, (64*player_pos[0], 64*player_pos[1]))
 
 
 
 if __name__ == "__main__":
 
     # Import images.
-    left_img = pygame.image.load("images/left1.png")
-    top_img = pygame.image.load("images/top1.png")
-    right_img = pygame.image.load("images/right1.png")
-    bottom_img = pygame.image.load("images/bottom1.png")
+    left_img: pygame.Surface = pygame.image.load("images/left1.png")
+    top_img: pygame.Surface = pygame.image.load("images/top1.png")
+    right_img: pygame.Surface = pygame.image.load("images/right1.png")
+    bottom_img: pygame.Surface = pygame.image.load("images/bottom1.png")
+    player_img: pygame.Surface = pygame.image.load("images/player.png")
+    goal_img: pygame.Surface = pygame.image.load("images/goal.png")
 
     if debug:
-        left_arrow = pygame.image.load("images/left_arrow.png")
-        top_arrow = pygame.image.load("images/top_arrow.png")
-        right_arrow = pygame.image.load("images/right_arrow.png")
-        bottom_arrow = pygame.image.load("images/bottom_arrow.png")
+        left_arrow: pygame.Surface = pygame.image.load("images/left_arrow.png")
+        top_arrow: pygame.Surface = pygame.image.load("images/top_arrow.png")
+        right_arrow: pygame.Surface = pygame.image.load("images/right_arrow.png")
+        bottom_arrow: pygame.Surface = pygame.image.load("images/bottom_arrow.png")
 
     # Set constants for the game.
     gridsize: tuple[int, int] = (10,10)
     gridsquare_size: int = 64
     
-    grid: Grid = generate_grid(gridsize)
+    grid: Grid = generateGrid(gridsize)
     movement: Movement = Movement()
-    W_pressed = False
-    A_pressed = False
-    S_pressed = False
-    D_pressed = False
+    W_pressed: bool = False
+    A_pressed: bool = False
+    S_pressed: bool = False
+    D_pressed: bool = False
+    score: int = 0
+    player_pos: list[int, int] = generatePlayerPos(gridsize)
+    goal_pos: tuple[int, int]  = generateGoalPos(gridsize, player_pos)
+    valid_directions: list[Direction] = []
 
     pygame.init()
 
-    display = pygame.display.set_mode((gridsize[0]*gridsquare_size, gridsize[1]*gridsquare_size))
+    display: pygame.Surface = pygame.display.set_mode((gridsize[0]*gridsquare_size, gridsize[1]*gridsquare_size))
     pygame.display.set_caption("Maze game")
 
     # Enter gameloop.
-    running = True
+    running: bool = True
     while running:
         # Set background to white and draw maze.
         display.fill((255, 255, 255))
-        draw_maze()
+        drawMaze()
 
-        movement.set_false()
+        movement.setAllFalse()
+        valid_directions = validMoveDirections(player_pos, grid)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                continue
 
             elif event.type == pygame.KEYDOWN:
                 # Only move once per key press, don't move multiple times if the key is held.
@@ -92,16 +102,34 @@ if __name__ == "__main__":
                     D_pressed = False
 
         # If multiple keys are pressed simultaneously, don't move.
-        if not movement.one_direction(): movement.set_false()
+        if not movement: movement.setAllFalse()            
 
         # Move the player.
-        if movement.left: 
-            if debug:
-                shift_origin(grid)
-                print(grid.origin)
-            print("Left")
-        if movement.right: print("Right")
-        if movement.top: print("Top")
-        if movement.bottom: print("Bottom")
+        if movement.left and (Direction.LEFT in valid_directions): 
+            player_pos[0] -= 1
+        elif movement.right and (Direction.RIGHT in valid_directions): 
+            player_pos[0] += 1
+        elif movement.top and (Direction.TOP in valid_directions): 
+            player_pos[1] -= 1
+        elif movement.bottom and (Direction.BOTTOM in valid_directions): 
+            player_pos[1] += 1
+        else:
+            # Disable movement if the direction input was not valid.
+            movement.setAllFalse()  
+
+        if movement:
+            # If only one key is input, do things.
+            shiftOrigin(grid)
+            score += 1
+
+            # Check win condition.
+            if tuple(player_pos) == goal_pos:
+                running = False
+                print(f"You won! Your final score is: {score}")
+
+            # Debug prints.
+            if debug: 
+                print(f"Origin: {grid.origin}")
+                print(f"Score: {score}")
 
         pygame.display.update()
